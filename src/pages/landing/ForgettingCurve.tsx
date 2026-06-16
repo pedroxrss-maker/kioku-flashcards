397d0f30e7721a8e2c7960de7ea69d4026603e3c
pedroxrss-maker <pedroxrss@gmail.com>
2026-06-16 08:10:17 -0300

    Atualizações

--- src/pages/landing/ForgettingCurve.tsx
+++ src/pages/landing/ForgettingCurve.tsx
@@ -228,7 +228,7 @@ export function ForgettingCurve() {
       </Reveal>
 
       <Reveal delay={0.1}>
-        <div className="surface mt-10 p-5 md:p-7 mx-auto" style={{ borderRadius: 'var(--r-lg)', maxWidth: 760 }}>
+        <div className="surface mt-10 p-5 md:p-7 mx-auto" style={{ borderRadius: 'var(--r-lg)', maxWidth: 836 }}>
           <Chart />
         </div>
       </Reveal>
